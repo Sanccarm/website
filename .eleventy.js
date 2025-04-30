4c3e57f53700b58802620147d5c7955fe552ce1c
sanccarm <sanccarm@mail.gvsu.edu>
2025-04-30 12:25:17 -0400

    Adding some more files for tracking. Updated eleventy.js to include css and resume folder for _site.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/resume");
+    eleventyConfig.addPassthroughCopy("css/style.css");
+
     return {
         dir: {
             input: "src",
-            output: "public",
-        };
+            output: "_site",
+        }
     };
 };
 
